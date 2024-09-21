808480086d3546fafc9169a88e4f7dcaeabe91c5
Excel18-coder <excel2130@gmail.com>
2024-09-21 05:01:32 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    char a ='k';
+    char name[]={"Excel"};
+    int age = 18;
+    float salary = 2000;
+    short area = 20;
+    int marks = 78;
+    printf("The character is %c\n" ,a);
+    printf("the name is %s\n" ,name);
+    printf("the age is  %d\n",age);
+    printf("the salary is %.2f\n",salary);
+    printf("the area is %d\n",area);
+    printf("EXCEL scored %d",marks);
+
+
+
+     return 0; 
+}
