b3c959a28a2e1c2d799c505f6ea8c8ae7091a8c3
Excel18-coder <excel2130@gmail.com>
2024-10-16 05:16:26 -0400

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int number,stop,sum;
+
+    printf("Enter the number to start: ");
+    scanf("%d",&number);
+
+    printf("Enter your stop number: ");
+    scanf("%d",&stop);
+
+    while(number<=stop){
+        printf("%d\n",number);
+        
+        number++;
+        
+        
+    
+
+    
+        
+
+        
+    }
+    printf("The sum of the numbsers is: %dc",sum);
+    return 0;
+}
